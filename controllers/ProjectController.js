26f50fea73c3a7e87aff9c3b01ce53f65f22856f
Thisari02 <thisarisiriwardana2@gmail.com>
2026-06-28 18:56:10 +0530

    get

--- controllers/ProjectController.js
+++ controllers/ProjectController.js
@@ -60,3 +60,16 @@ exports.deleteProject = async (req, res) => {
     res.status(400).json({ error: error.message });
   }
 };
+
+exports.getProjectById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const project = await Project.findById(id).lean();
+       if (!project) {
+      return res.status(404).json({ error: 'Project not found' });
+    }
+    res.json(project);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
