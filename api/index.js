d75f8b37db5f7595c00136a7b6ee625a4ff85959
Thisari02 <thisarisiriwardana2@gmail.com>
2026-06-28 19:06:53 +0530

    test

--- api/index.js
+++ api/index.js
@@ -11,7 +11,8 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 app.use(async (req, res, next) => {
   try {
